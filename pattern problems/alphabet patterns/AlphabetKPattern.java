94cac8d832e051c2e95f00b1171eb2dfb78bf350
srikanth <msrikanth.ink@gmail.com>
2026-04-14 22:44:21 +0530

    added

---
+++ pattern problems/alphabet patterns/AlphabetKPattern.java
@@ -0,0 +1,25 @@
+/*
+
+input = 5
+
+sample output:
+
+A B C D E 
+A B C D 
+A B C 
+A B 
+A 
+A B
+A B C
+A B C D
+A B C D E
+
+*/
+
+import java.util.Scanner;
+
+public class AlphabetKPattern {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+  }
+}
