6b36deda520a14b58d2810e5c1e21e0dcfcba4ca
srikanth <msrikanth.ink@gmail.com>
2026-04-15 12:23:32 +0530

    alphabet k-pattern code is updated

--- pattern problems/alphabet patterns/AlphabetKPattern.java
+++ pattern problems/alphabet patterns/AlphabetKPattern.java
@@ -21,5 +21,15 @@ import java.util.Scanner;
 public class AlphabetKPattern {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for (int i = 0; i < 2 * n - 1; i++) {
+      int max_cols = (i < n) ? n - i : i - n + 2;
+
+      for (int j = 0; j < max_cols; j++) {
+        System.out.print(((char) (64 + j + 1)) + " ");
+      }
+      System.out.println();
+    }
   }
 }
